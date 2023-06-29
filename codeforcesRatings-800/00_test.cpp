0d5549742dfbfb25b7ff775f05f56c419daa7736
mostafizz-fuad <mostafizzfuad@gmail.com>
2023-06-30 00:46:22 +0600

    update code

---
+++ codeforcesRatings-800/00_test.cpp
@@ -0,0 +1,11 @@
+/// created by mostafizzfuad
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+int main() {
+
+    
+
+    return 0;
+}
---
+++ codeforcesRatings-800/watermelon.cpp
@@ -0,0 +1,15 @@
+/// created by mostafizzfuad
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+int main() {
+
+    int w;
+    cin >> w;
+
+    if (w % 2 == 1  || w == 2) cout << "NO" << endl;
+    else cout << "YES" << endl;
+
+    return 0;
+}
