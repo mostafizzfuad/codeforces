2a4a4aee5702b4cc96df63ae1ce14ea49373b1f5
mostafizz-fuad <mostafizzfuad@gmail.com>
2023-07-09 01:36:41 +0600

    update code

--- codeforcesSolution-A/00_test.cpp
+++ codeforcesSolution-A/00_test.cpp
@@ -5,19 +5,7 @@ using namespace std;
 #define ld long double
 int main() {
 
-    int n, k, ans = 0;
-    cin >> n >> k;
-
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
-    for (int i = 0; i < n; i++) {
-        if (arr[i] >= arr[k - 1] &&  arr[i] > 0) ans++;
-    }
-
-    cout << ans << endl;
+    
 
     return 0;
 }
