9bac9cbda6100447b0315c1b134f930c9a2b1451
mostafizz-fuad <mostafizzfuad@gmail.com>
2023-07-09 01:34:59 +0600

    solved three problems

---
+++ codeforcesSolution-A/1A_theatreSquare.cpp
@@ -0,0 +1,22 @@
+/// created by mostafizzfuad
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+int main() {
+
+    ll m, n, a, p, q;
+    cin >> m >> n >> a;
+
+    /// for m
+    if (m % a == 0) p = m / a;
+    else p = (m / a) + 1;
+
+    /// for n
+    if (n % a == 0) q = n / a;
+    else q = (n / a) + 1;
+
+    cout << p * q << endl;
+
+    return 0;
+}
---
+++ codeforcesSolution-A/50A_dominoPiling.cpp
@@ -0,0 +1,15 @@
+/// created by mostafizzfuad
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+int main() {
+
+    int m, n;
+    cin >> m >> n;
+
+    int ans = (m * n) / 2;
+    cout << ans << endl;
+
+    return 0;
+}
