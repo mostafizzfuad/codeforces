7d7c6e2c2eae83add64e3b9e76295554a2844bec
mostafizz-fuad <mostafizzfuad@gmail.com>
2023-07-22 01:39:31 +0600

    update code

--- 00_test.cpp
+++ 00_test.cpp
@@ -5,20 +5,6 @@ using namespace std;
 #define ld long double
 int main() {
 
-    int t, sumOne = 0, sumTwo = 0, sumThree = 0;
-    cin >> t;
-
-    int arr[100][100];
-    for (int i = 0; i < t; i++) {
-        for (int j = 0; j < 3; j++) {
-            cin >> arr[i][j];
-        }
-        sumOne += arr[i][0];
-        sumTwo += arr[i][1];
-        sumThree += arr[i][2];
-    }
-
-    if (sumOne == 0 && sumTwo == 0 && sumThree == 0) cout << "YES" << endl;
-    else cout << "NO" << endl;
+    
     
 }
---
+++ codeforcesSolution-A/1703A_YES or YES.cpp
@@ -0,0 +1,19 @@
+/// created by mostafizzfuad
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string s;
+        cin >> s;
+
+        if ((s[0] == 'Y' || s[0] == 'y') && (s[1] == 'E' || s[1] == 'e') && (s[2] == 'S' || s[2] == 's')) cout << "YES" << endl;
+        else cout << "NO" << endl; 
+    }
+    
+}
