3d7abe00d864a320300b1b751003aecc0ce6863a
mostafizz-fuad <mostafizzfuad@gmail.com>
2023-07-21 15:54:20 +0600

    update code

---
+++ codeforcesSolution-A/1626A_equidistantLetters.cpp
@@ -0,0 +1,19 @@
+/// created by mostafizzfuad
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string s;
+        cin >> s;
+        
+        sort(s.begin(), s.end());
+        cout << s << endl;
+    }
+
+}
